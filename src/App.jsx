01a55f4dbbeca17c91120183d6bb15934b6c2793
Riya Saini <rsaini@opentext.com>
2025-05-25 18:04:42 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Body from './components/Body';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter basename='/'>
-      <Routes>
-        <Route path="/" element={<Body/>}/>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
