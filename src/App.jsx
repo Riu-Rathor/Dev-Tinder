067fa9f429da04b6ce70ab293b1219432b0871d2
Riya Saini <rsaini@opentext.com>
2025-05-25 17:50:58 +0530

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 function App() {
 
   return (
     <>
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
     <Navbar/>
     </>
   )
